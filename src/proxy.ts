6155c3f868fc943008631b0165d3c8bedd044318
Max <93175059+MaxwellMorrow@users.noreply.github.com>
2025-12-14 12:32:30 -0700

    Remove middleware for authentication and authorization checks

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
 export default withAuth(
-  function middleware(req) {
+  function proxy(req) {
     const token = req.nextauth.token;
     const isAdminRoute = req.nextUrl.pathname.startsWith("/admin");
 
@@ -44,4 +44,3 @@ export const config = {
     "/account/:path*",
   ],
 };
-
